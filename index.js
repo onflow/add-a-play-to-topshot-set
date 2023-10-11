10187e9df6bb1a71b81ccf35e1d830d4a4720fe1
lealobanov <lea.lobanov@icloud.com>
2023-10-11 23:18:44 +0900

    Playground links

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ export const addAPlayToTopShotSet= {
   createdAt: Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
-    "",
+  "https://play.onflow.org/15c1e86e-010c-4a7c-bcfd-98a1bddc36a7?type=tx&id=c6627028-4fbc-416a-b8d7-0c433e6a85f6&storage=none",
   excerpt:
-    "",
+    "Once you have a set created and some plays, you can use this to add a play to a TopShot Set that can later be minted.",
   smartContractCode: contractPath,
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
