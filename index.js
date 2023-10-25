dda699701db8b1e9d8b99adfe820d6740e5be908
lealobanov <lea.lobanov@icloud.com>
2023-10-26 00:26:16 +0900

    Create filters and assign difficulty

--- index.js
+++ index.js
@@ -22,5 +22,8 @@ export const addAPlayToTopShotSet= {
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
   transactionExplanation: transactionExplanationPath,
+  filters: {
+    difficulty: "intermediate"
+  }
 };
 
